ea9b87136d2e7e955eb667a32279e7392f339b6e
diyong6334 <diyongstephen.com>
2021-05-31 09:16:28 +0100

    adding contact form

--- run.py
+++ run.py
@@ -1,15 +1,34 @@
 
+from os import name
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
  
  
 #create the object of Flask
 app  = Flask(__name__)
+app.secret_key = "secret key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root: '' @localhost/crud'
+app.config['SQLALCHEMY_TRACK_MODIFICATION']= False
+db = SQLAlchemy(app)
+
+#creating Table
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(120), unique=True, nullable=False)
+    def __init__(self, name, email,phone):
+        self.name = name
+        self.email = email
+        self.phone = phone 
  
 #creating our routes
 @app.route('/')
 def index():
     return render_template('/index.html')
 
+#creating our about routes
 @app.route('/about')
 def about():
     return render_template('/about.html')
@@ -39,10 +58,8 @@ def services():
 def news():
     return render_template('/news.html')
 
-
-
-
  
 #run flask app
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
